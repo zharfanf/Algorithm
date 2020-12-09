d93454f537d091ef626ba4390e952cb0ba4b1e69
Faishal Zharfan <zharfanf@gmail.com>
2020-12-09 13:47:32 +0700

    Update bubblesort.py
    Optimizing Algorithm

--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -1,23 +1,10 @@
-def is_sort(arr):
-	sort = True
-	i = 1
-	while sort and i < len(arr):
-		if arr[i-1] > arr[i]:
-			sort = False
-		i += 1
-	return sort
-
-
-def bubblesort(arr):
-	while not(is_sort(arr)):
+def bubblesort_test(arr):
+	for i in range(len(arr)-1,-1,-1):
 		pivot = arr[-1]
-		for i in range(len(arr)-2,-1,-1):
-			if pivot < arr[i]:
-				arr[i+1] = arr[i]
-				arr[i] = pivot
-
+		for j in range(len(arr)-2,len(arr)-2-i,-1):
+			if pivot < arr[j]:
+				arr[j+1] = arr[j]
+				arr[j] = pivot
 			else:
-				pivot = arr[i]
+				pivot = arr[j]
 	return arr
-
-print(bubblesort([7,1,3,0,9,8,4]))
